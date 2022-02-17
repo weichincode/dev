92fb9ab14521eb33fb4ade6ffce73b5d6e9dd1fd
firedrake344 <firedrake344.code@gmail.com>
2022-02-17 11:00:48 +0800

    up

---
+++ wishCalc/wishCalc.py
@@ -0,0 +1,14 @@
+currentWish = 90
+# History
+pages = 9
+singles = 0
+# Days to new Character Banner
+daysToNextPatch = 19 + 21 + 21
+
+pity = (pages*6) + singles
+
+wishAccumulated = (15/16) * daysToNextPatch
+
+totalWish = currentWish + pity + wishAccumulated
+
+print(totalWish)
